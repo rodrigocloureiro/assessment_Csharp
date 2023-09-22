95f391c815ceb0af34539a7d6c59419736a4dd77
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-22 14:15:46 -0300

    Propriedades classe Aluno

--- CamadaDeNegocio/Aluno.cs
+++ CamadaDeNegocio/Aluno.cs
@@ -14,6 +14,13 @@
             _dataNascimento = dataNascimento;
             _mediaFinal = mediaFinal;
         }
+
+        public string Nome { get { return _nome; } }
+        public DateTime DataNasicmento { get { return _dataNascimento; } }
+        public bool Aprovado { get { return _aprovado; } }
+        public double MediaFinal { get { return _mediaFinal; } }
+        public Guid Id { get { return _id; } }
+
     }
 }
 
