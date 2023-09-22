e630fbf89d31e6b09c4d666ecaa6a32276424a5b
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-22 15:14:51 -0300

    delegate e menu de escolha de operacao

--- assessment/Program.cs
+++ assessment/Program.cs
@@ -3,15 +3,40 @@
 namespace assessment;
 public class Program
 {
+    public delegate IRepositorio Repositorios();
+    //public IRepositorio _repositorio;
+    public static IRepositorio _repositorio;
+
+    public static void TipoOperacao()
+    {
+        Repositorios repositorios;
+
+        Console.WriteLine("Como deseja salvar?");
+        Console.WriteLine("1- Em lista");
+        Console.WriteLine("2- Em arquivo");
+        int operacao = int.Parse(Console.ReadLine());
+
+        if (operacao == 1) repositorios = () => new RepositorioLista();
+        else if (operacao == 2) return;
+        else
+        {
+            Console.WriteLine("Opção inválida.");
+            return;
+        }
+
+        //new Program()._repositorio = repositorios();
+        _repositorio = repositorios();
+    }
+
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-
         string pathFile = "alunos.csv";
 
-        Aluno al = new Aluno("Rodrigo Loureiro", DateTime.ParseExact("01/03/2001", "dd/MM/yyyy", CultureInfo.CurrentCulture), 9.1);
+        TipoOperacao();
 
-        Console.WriteLine(al);
+        //_repositorio.Salvar(new Aluno("Rodrigo Loureiro", DateTime.ParseExact("01/03/2001", "dd/MM/yyyy", CultureInfo.CurrentCulture), 9.1));
+
+        //_repositorio.Listar().ForEach(al => Console.WriteLine(al));
 
         Console.ReadKey();
     }
