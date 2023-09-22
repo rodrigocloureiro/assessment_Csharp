e472b2aba6dd1a5294fbc8ab045b384f6ff69556
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-22 14:15:23 -0300

    Classe Aluno criada

---
+++ CamadaDeNegocio/Aluno.cs
@@ -0,0 +1,19 @@
+﻿namespace assessment
+{
+    public class Aluno
+    {
+        private string _nome;
+        private DateTime _dataNascimento;
+        private bool _aprovado;
+        private double _mediaFinal;
+        private Guid _id;
+
+        public Aluno(string nome, DateTime dataNascimento, double mediaFinal)
+        {
+            _nome = nome;
+            _dataNascimento = dataNascimento;
+            _mediaFinal = mediaFinal;
+        }
+    }
+}
+
--- assessment/Program.cs
+++ assessment/Program.cs
@@ -4,5 +4,9 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        string pathFile = "alunos.csv";
+
+        Console.ReadKey();
     }
 }
