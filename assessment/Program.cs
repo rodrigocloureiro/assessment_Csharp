3e65feca24e59c78cb47c09ff2fc2538c520513c
rodrigocloureiro <rodrigocostaloureiro563@gmail.com>
2023-09-26 14:10:48 -0300

    Program refatorado

--- assessment/Program.cs
+++ assessment/Program.cs
@@ -1,106 +1,106 @@
-﻿namespace assessment;
-public class Program
+﻿namespace assessment
 {
-    public delegate IRepositorio Repositorios();
-    public static IRepositorio _repositorio; //public IRepositorio _repositorio;
-
-    public static void TipoOperacao()
+    public class Program
     {
-        Repositorios repositorios;
+        public delegate IRepositorio Repositorio();
+        public static IRepositorio _repositorio; //public IRepositorio _repositorio;
 
-        Console.Clear();
-        Console.WriteLine("Como deseja salvar?");
-        Console.WriteLine("1- Em lista");
-        Console.WriteLine("2- Em arquivo");
-        int operacao = int.Parse(Console.ReadLine());
-
-        if (operacao == 1) repositorios = () => new RepositorioEmColecao();
-        else if (operacao == 2) repositorios = () => new RepositorioEmArquivo();
-        else throw new Exception("Opção inválida.");
-
-        _repositorio = repositorios(); //new Program()._repositorio = repositorios();
-    }
-
-    public static void ExibirUltimosCadastros()
-    {
-        Console.Clear();
-
-        List<Aluno> alunos = _repositorio.Listar();
-        List<Aluno> ultimosCadastros = alunos.Count >= 5 ? alunos.GetRange(alunos.Count - 5, 5) : alunos;
-
-        if (ultimosCadastros.Count > 0 && ultimosCadastros.Count < 5)
-            Console.WriteLine($"Há um total de {ultimosCadastros.Count} alunos cadastrados.");
-        else if (ultimosCadastros.Count > 0)
+        public static void TipoOperacao()
         {
-            Console.WriteLine("Últimos 5 cadastros:\n");
-            ultimosCadastros.ForEach(al => Console.WriteLine(al));
+            Repositorio repo = null;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Como deseja salvar?");
+                Console.WriteLine("1- Em lista");
+                Console.WriteLine("2- Em arquivo");
+                int.TryParse(Console.ReadLine(), out int operacao);
+
+                switch (operacao)
+                {
+                    case 1:
+                        repo = () => new RepositorioEmColecao();
+                        //_repositorio = new RepositorioEmColecao();
+                        break;
+                    case 2:
+                        repo = () => new RepositorioEmArquivo();
+                        //_repositorio = new RepositorioEmArquivo();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Aperte ENTER e tente novamente.");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (repo == null);
+
+            _repositorio = repo(); //new Program()._repositorio = repositorios();
         }
-        else Console.WriteLine("Ainda não há alunos cadastrados.");
 
-        Console.WriteLine("Aperte ENTER para continuar");
-        Console.ReadKey();
-    }
-
-    public static void GerandoLog(Aluno aluno, RepositorioEventArgs e)
-    {
-        Console.WriteLine($"O aluno {aluno.Nome} foi {e.Acao}");
-    }
-
-    public static void Main(string[] args)
-    {
-        do
-        {
-            try
-            {
-                TipoOperacao();
-                ExibirUltimosCadastros();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.ReadKey();
-            }
-        } while (_repositorio == null);
-
-        bool prosseguir = true;
-        Menu menu = new Menu(_repositorio);
-        _repositorio.RepositoryChanged += GerandoLog;
-
-        do
+        public static void UltimosCadastros()
         {
             Console.Clear();
-            Console.WriteLine("1- Pesquisar aluno"); // Read
-            Console.WriteLine("2- Adicionar aluno"); // Create
-            Console.WriteLine("3- Editar aluno"); // Update
-            Console.WriteLine("4- Apagar aluno"); // Delete
-            Console.WriteLine("5- Sair");
-            int opcao = int.Parse(Console.ReadLine());
+            List<Aluno> ultimosCadastros = _repositorio.Listar();
 
-            switch (opcao)
+            if (ultimosCadastros.Count >= 5)
             {
-                case 1:
-                    menu.ListarAluno();
-                    break;
-                case 2:
-                    menu.AdicionarAluno();
-                    break;
-                case 3:
-                    menu.EditarAluno();
-                    break;
-                case 4:
-                    menu.ApagarAluno();
-                    break;
-                case 5:
-                    prosseguir = false;
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida. Aperte ENTER e tente novamente.");
-                    Console.ReadKey();
-                    break;
+                Console.WriteLine("***** Últimos 5 cadastros *****\n");
+                ultimosCadastros.GetRange(_repositorio.Listar().Count - 5, 5).ForEach(al => Console.WriteLine(al));
             }
+            else if (ultimosCadastros.Count > 0) Console.WriteLine($"Há um total de {ultimosCadastros.Count} alunos cadastrados.");
+            else Console.WriteLine("Não há alunos cadastrados.");
 
-        } while (prosseguir);
+            Console.WriteLine("Aperte ENTER para continuar");
+            Console.ReadKey();
+        }
 
-        Console.ReadKey();
+        public static void Log(Aluno aluno, RepositorioEventArgs e)
+        {
+            Console.WriteLine($"O aluno {aluno.Nome} foi {e.Acao}");
+        }
+
+        public static void Main(string[] args)
+        {
+            TipoOperacao();
+            UltimosCadastros();
+            bool prosseguir = true;
+            Menu menu = new Menu(_repositorio);
+            _repositorio.RepositoryChanged += Log;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1- Pesquisar aluno"); // Read
+                Console.WriteLine("2- Adicionar aluno"); // Create
+                Console.WriteLine("3- Editar aluno"); // Update
+                Console.WriteLine("4- Apagar aluno"); // Delete
+                Console.WriteLine("5- Sair");
+                int.TryParse(Console.ReadLine(), out int opcao);
+
+                switch (opcao)
+                {
+                    case 1:
+                        menu.ListarAluno();
+                        break;
+                    case 2:
+                        menu.AdicionarAluno();
+                        break;
+                    case 3:
+                        menu.EditarAluno();
+                        break;
+                    case 4:
+                        menu.ApagarAluno();
+                        break;
+                    case 5:
+                        prosseguir = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Aperte ENTER e tente novamente.");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (prosseguir);
+            Console.ReadKey();
+        }
     }
 }
